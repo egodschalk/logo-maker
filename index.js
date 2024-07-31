970841d39859ab199905ca7003416010ad2e703f
egodschalk <egodschalk@gmail.com>
2024-07-31 12:11:04 -0400

    final commit

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ class Questions {
         return inquirer.prompt([
             {
                 type: "input",
-                message: "Please enter 3 letters for the text of your logo",
+                message: "Please enter up to 3 letters for the text of your logo:",
                 name: "logoText"
             },
             {
                 type: "input",
-                message: "Please enter the text color for your logo, using either the color name or a hexidecimal code",
+                message: "Please enter the text color for your logo (color name or hex code):",
                 name: "textColor",
             },
             {
                 type: "list",
-                message: "Please choose the shape you would like to use for your logo",
+                message: "Please choose the shape you would like to use for your logo:",
                 name: "logoShape",
                 choices: ["circle", "square", "triangle"],
             },
             {
                 type: "input",
-                message: "Please enter the shape color for your logo, using either the color name or a hexidecimal code",
+                message: "Please enter the shape color for your logo (color name or hex code):",
                 name: "shapeColor",
             },
         ])
@@ -60,24 +60,3 @@ class Questions {
 }
     
 new Questions().run()
-
-
-// function writeToFile(fileName, answers) {
-//     fs.writeFile('logo.svg', generateMarkdown(answers), (err) =>
-//         err ? console.error(err) : console.log('Success!')
-//       );
-//  }
-
-//  function init() { 
-//     inquirer.prompt(questions).then(answers => {
-//         writeToFile('logo.svg', answers)
-//     })
-// }
-
-// // Function call to initialize app
-// init();
-
-
-
-
-
